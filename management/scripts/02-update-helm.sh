1d6a9f88b58c0cc9a090d164ac2dc8009fa68473
seunghayim <ha@DESKTOP-7OULA8F.localdomain>
2022-06-04 00:24:56 +0900

    Add management/scripts/02-update-helm.sh kubectl authcomplete

--- management/scripts/02-update-helm.sh
+++ management/scripts/02-update-helm.sh
@@ -11,3 +11,8 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm help
+
+
+# Kubectl autocomplete
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
